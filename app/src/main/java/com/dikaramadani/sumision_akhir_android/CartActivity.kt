05f8e2751ed4debc56085f8f7b55a92387aaf2ae
dika ramadani <dikabangkok21@gmail.com>
2025-11-01 19:53:54 +0700

    Add initial implementation of Cart, History, and related UI components

---
+++ app/src/main/java/com/dikaramadani/sumision_akhir_android/CartActivity.kt
@@ -0,0 +1,13 @@
+package com.dikaramadani.sumision_akhir_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/dikaramadani/sumision_akhir_android/HistoryAdapter.kt
@@ -0,0 +1,20 @@
+package com.dikaramadani.sumision_akhir_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HistoryAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_history_adapter)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
